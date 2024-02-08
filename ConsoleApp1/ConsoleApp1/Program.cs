0482f6433f6d649cf926d7871287ba49cdfc4df9
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2024-02-08 17:57:09 +0200

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,11 +15,89 @@ namespace ConsoleApp1
             //    Console.WriteLine($"x = {x}\ty = {-5 * Math.Pow(x, 2) + 2 * x + 1}");
             //}
 
+            //Вариант 1
+            //for(double x = -1; x <= 1; x += 0.1)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {3 * Math.Pow(x, 2):F2}");
+            //}
+
+            //Вариант 2
+            //for (double x = -2; x <= 2; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Pow(x, 3):F3}");
+            //}
+
+            //Вариант 3
+            //for (double x = 0; x <= 5; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Sqrt(x):F3}");
+            //}
+
+            //Вариант 4
+            //for (double x = -3.14; x <= 3.14; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F2} \ty = {Math.Cos(x):F3}");
+            //}
+
+            //Вариант 5
+            //for (double x = 1; x <= 12; x += 0.5)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {12/x:F2}");
+            //}
+
+            //Вариант 6
+            //for (double x = -1.57; x <= 1.57; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F2} \ty = {Math.Atan(x):F3}");
+            //}
+
             //Вариант 7
-            for (double x = 0; x <= 2; x += 0.2)
-            {
-                Console.WriteLine($"x = {x}\ty = {Math.Sqrt(x) / (x + 1)}");
-            }
+            //for (double x = 0; x <= 2; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Sqrt(x) / (x + 1):F2}");
+            //}
+
+            //Вариант 8
+            //for (double x = -3.14; x <= 3.14; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F2} \ty = {Math.Sin(x):F3}");
+            //}
+
+            //Вариант 9
+            //for (double x = -3; x <= 3; x += 0.5)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Pow(x,2)/2+3:F2}");
+            //}
+
+            //Вариант 10
+            //for (double x = 0; x <= 8; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Pow(x-5, 2)/2:F2}");
+            //}
+
+            //Вариант 11
+            //for (double x = -5; x <= 5; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Pow(x,2)/4+x+1:F2}");
+            //}
+
+            //Вариант 12
+            //for (double x = -2; x <= 7; x += 0.1)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Pow(x-3,2)/2+2:F2}");
+            //}
+
+            //Вариант 13
+            //for (double x = -3; x <= 3; x += 0.2)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Abs(x):F2}");
+            //}
+
+            //Вариант 14
+            //for (double x = -3; x <= 3; x += 0.1)
+            //{
+            //    Console.WriteLine($"x = {x:F1} \ty = {Math.Pow(x,4)/8 - Math.Pow(x,2):F2}");
+            //}
 
             Console.ReadKey();
         }
